494ca0ae346a37f9a09d3e4255a255b2f58ccca5
Hosung.Kim <hosungkim5108@gmail.com>
2025-02-25 15:08:12 +0900

    [Environment] 프로젝트 최초 세팅

---
+++ DuckStar/DuckStar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DuckStar
+//
+//  Created by 김호성 on 2025.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
